35f4a4fff6aee3cd156fff53b199817029ab7a44
marhe <marhe@cn.ibm.com>
2018-04-06 20:33:34 +0800

    commit

--- test/app/src/main/java/logan/dl/com/myapplication/fragment/Fragment1.java
+++ test/app/src/main/java/logan/dl/com/myapplication/fragment/Fragment1.java
@@ -1,22 +1,25 @@
 package logan.dl.com.myapplication.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import logan.dl.com.myapplication.R;
+import logan.dl.com.myapplication.ShowMapActivity;
 
 
-public class Fragment1 extends Fragment {
-
-
+public class Fragment1 extends Fragment implements View.OnClickListener{
 
     private View view=null;
     private Context context;
+
+    private TextView tv_findchewei;
 
     @Nullable
     @Override
@@ -24,10 +27,21 @@ public class Fragment1 extends Fragment {
         context = getActivity();
 
         view=inflater.inflate(R.layout.fragment1,container,false);
-
-
+        tv_findchewei = (TextView) view.findViewById(R.id.zhaochewei);
+        tv_findchewei.setOnClickListener(this);
         return view;
     }
 
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.zhaochewei:
+                Intent intent = new Intent(getActivity(),ShowMapActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                    break;
+        }
+    }
 }
